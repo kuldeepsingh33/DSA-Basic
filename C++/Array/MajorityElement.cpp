8221d23bbea8983aa8f27e01e0c2f67ebfcc8184
Ayush Pandey <60148537+ayushpandey830@users.noreply.github.com>
2022-10-15 23:12:51 +0530

    Create MajorityElement.cpp
    Solution to Problem Majority Element in Cpp.

---
+++ C++/Array/MajorityElement.cpp
@@ -0,0 +1,27 @@
+void findMajority(int arr[], int n)
+{
+    int maxCount = 0;
+    int index = -1; // sentinels
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j])
+                count++;
+        }
+ 
+        // update maxCount if count of
+        // current element is greater
+        if (count > maxCount) {
+            maxCount = count;
+            index = i;
+        }
+    }
+ 
+    // if maxCount is greater than n/2
+    // return the corresponding element
+    if (maxCount > n / 2)
+        cout << arr[index] << endl;
+ 
+    else
+        cout << "No Majority Element" << endl;
+}
